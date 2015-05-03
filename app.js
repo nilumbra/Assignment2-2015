d0bf0cd9ac2df2e3170cb22ea7ff6bc9bcc2863b
nilumbra <huc005@ucsd.edu>
2015-05-03 05:26:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
 
 //connect to database
-mongoose.connect(process.env.MONGODB_CONNECTION_LOCAL);
+mongoose.connect(process.env.MONGODB_CONNECTION_URL);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
